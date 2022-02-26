6b6e2e4eebe26e74f21598e10c87b03775f8cc96
Gayoung <gyyoon4u@naver.com>
2022-02-26 23:58:27 +0900

    입문_section06_h2 db설치

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String hoeme() {
+    public String home() {
         return "home";
     }
 }
